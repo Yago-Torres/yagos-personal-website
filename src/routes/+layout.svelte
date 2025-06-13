bba109f2f7f09b5850c257eaf2526c824391f94c
Yago-Torres <torresgarciayago@gmail.com>
2025-06-13 23:14:59 +0200

    test8

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,13 +8,7 @@
   <link rel="icon" href={base + '/favicon.png'} />
 
   <!-- Cloudflare Web Analytics beacon -->
-  <script
-    defer
-    src="https://static.cloudflareinsights.com/beacon.min.js"
-    data-cf-beacon={JSON.stringify({
-      token: '950e2fe0c7e64a23b943112b4c16d1b9'
-    })}
-  	></script>
+  
 </svelte:head>
 
 <slot />
