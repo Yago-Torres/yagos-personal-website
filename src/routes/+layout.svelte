ba90872afa5caf889ba0ebe1e6e06d7b36ebcdf7
Yago-Torres <torresgarciayago@gmail.com>
2025-06-13 12:06:04 +0200

    favicon changed + code updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
+import { base } from '$app/paths';
+
 <script lang="ts">
+    import { base } from '$app/paths';
 	import '../app.css';
 	
 	let { children } = $props();
 </script>
+
+<link rel="icon" href={base+'/favicon.ico'} />
 
 {@render children()}
--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		paths: {
+			// This is needed for the adapter to work correctly with Cloudflare Workers.
+			// It ensures that the paths are correctly resolved in the production environment.
+			base: process.env.NODE_ENV === 'cloudflare_pages' ? '' : '',
+			assets: process.env.NODE_ENV === 'cloudflare_pages' ? '' : ''
+		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
