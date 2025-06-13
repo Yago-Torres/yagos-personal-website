527b4e9da26de194fcd28afdd1968578b64e5c34
Yago-Torres <torresgarciayago@gmail.com>
2025-06-13 15:39:07 +0200

    moved analytics to head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,14 @@
 
 <svelte:head>
   <!-- favicon lives in your `static/` folder -->
-  <link rel="icon" href="{base}/favicon.png" />
-</svelte:head>
+  <link rel="icon" href={base + '/favicon.png'} />
 
-{@html `
-  <script 
+  <!-- Cloudflare Web Analytics beacon -->
+  <script
     defer
-    src="https://static.cloudflareinsights.com/beacon.min.js"
-    data-cf-beacon='{"token":"950e2fe0c7e64a23b943112b4c16d1b9"}'>
-  </script>
-`}
+    src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015"
+    data-cf-beacon={JSON.stringify({ token: '950e2fe0c7e64a23b943112b4c16d1b9' })}
+  	></script>
+</svelte:head>
 
 <slot />
