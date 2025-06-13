6b4ab01e9b122e1ddbd099dc231c102863bbf7c1
Yago-Torres <torresgarciayago@gmail.com>
2025-06-13 18:52:18 +0200

    reverted

--- svelte.config.js
+++ svelte.config.js
@@ -17,22 +17,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		csp: {
-      		mode: 'auto',
-      		directives: {
-        		defaultSrc: ["'self'"],
-        		scriptSrc: [
-          			"'self'",
-          			'https://static.cloudflareinsights.com'  // allow the beacon script
-        		],
-        		styleSrc: ["'self'", "'unsafe-inline'"],
-        		imgSrc: ["'self'", 'data:'],
-        		connectSrc: ["'self'", 'https://static.cloudflareinsights.com'],
-        		fontSrc: ["'self'"],
-        		frameSrc: ["'self'"]
-      		}
-    	}
+		adapter: adapter()
 	}
 };
 
