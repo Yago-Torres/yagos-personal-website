3406643c1c3e391cf0741add02bddb5a925176ea
Yago-Torres <torresgarciayago@gmail.com>
2025-06-14 13:55:46 +0200

    TENEMOS CITIZEN

--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -1,58 +1,75 @@
-<script>
-  import { T, useTask } from '@threlte/core'
-  import { interactivity } from '@threlte/extras'
-  import { Spring } from 'svelte/motion'
-    import { MTLLoader, OBJLoader } from 'three-stdlib';
-  import shader from '$lib/utils/shaders.glsl?raw'
+<!-- src/lib/Scene.svelte -->
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { T, extend, useTask } from '@threlte/core';
+  import { interactivity } from '@threlte/extras';
+  import { Spring } from 'svelte/motion';
+  import { MTLLoader, OBJLoader } from 'three-stdlib';
+  import * as THREE from 'three';
 
-  interactivity()
+  // register all THREE classes so <T.primitive> knows what to do
+  extend(THREE);
 
-  const scale = new Spring(1)
+  interactivity();
 
-  let rotation = 0
-  useTask((delta) => {
-    rotation += delta
-  })
+  // hover‐scale spring
+  const scale = new Spring(1);
+
+  // rotation
+  let rotation = 0;
+  useTask((delta) => (rotation += delta));
+
+  // loaded model goes here
+  let citizenModel: THREE.Group | null = null;
+
+  onMount(() => {
+    const mtlLoader = new MTLLoader();
+    mtlLoader.load(
+      '/models/citizen.mtl',
+      (materials) => {
+        materials.preload();
+        const objLoader = new OBJLoader();
+        objLoader.setMaterials(materials);
+        objLoader.load(
+          '/models/citizen.obj',
+          (group) => (citizenModel = group),
+          undefined,
+          (err) => console.error(err)
+        );
+      },
+      undefined,
+      (err) => console.error(err)
+    );
+  });
 </script>
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[10, 10, 10]}
-  oncreate={(ref) => {
-    ref.lookAt(0, 1, 0)
-  }}
-/>
+{#if citizenModel}
+  <T.PerspectiveCamera
+    makeDefault
+    position={[10, 10, 10]}
+    oncreate={(cam) => cam.lookAt(0, 1, 0)}
+  />
+  <T.DirectionalLight position={[0, 10, 10]} castShadow />
 
-<T.DirectionalLight
-  position={[0, 10, 10]}
-  castShadow
-/>
+  <!-- now Threlte knows about THREE.Group, Mesh, Materials, etc. -->
 
-<T.Mesh
-  rotation.y={rotation}
-  position.y={1}
-  scale={scale.current}
-  onpointerenter={() => {
-    scale.target = 1.5
-  }}
-  onpointerleave={() => {
-    scale.target = 1
-  }}
-  castShadow
->
-<T.ShaderMaterial
-		{shader}
-		
-	></T.ShaderMaterial>
+  <T
+    is={citizenModel}
+    rotation-y={rotation}
+    position-y={1}
+    scale={scale.current}
+    castShadow
+    receiveShadow
+    onpointerenter={() => scale.target = 1.5}
+    onpointerleave={() => scale.target = 1}
+  />
 
-  <T.BoxGeometry args={[1, 2, 1]} />
-  <T.MeshStandardMaterial color="hotpink" />
-</T.Mesh>
-delta
-<T.Mesh
-  rotation.x={-Math.PI / 2}
-  receiveShadow
->
-  <T.CircleGeometry args={[4, 40]} />
-  <T.MeshStandardMaterial color="white" />
-</T.Mesh>
+    <T.Mesh rotation-x={-Math.PI / 2} receiveShadow>
+    <T.CircleGeometry args={[4, 40]} />
+    <T.MeshStandardMaterial color="white" />
+  </T.Mesh>
+{:else}
+  <div style="position: absolute; top: 1rem; left: 1rem; color: white">
+    Loading model…
+  </div>
+{/if}
